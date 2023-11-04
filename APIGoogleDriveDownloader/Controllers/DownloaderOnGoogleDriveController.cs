2981927b4305efad88b4f9d7ec47e70cc0013484
freshhuk <freshhuk13@gmail.com>
2023-11-04 22:15:18 +0200

    Добавьте файлы проекта.

---
+++ APIGoogleDriveDownloader/Controllers/DownloaderOnGoogleDriveController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Auth.OAuth2;
+
+namespace APIGoogleDriveDownloader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloaderOnGoogleDriveController : ControllerBase
+    {
+        
+        public static string DriveUploadBasic(string filePath)
+        {
+            try
+            {
+                /* Load pre-authorized user credentials from the environment.
+                 TODO(developer) - See https://developers.google.com/identity for
+                 guides on implementing OAuth2 for your application. */
+
+                GoogleCredential credential = GoogleCredential.GetApplicationDefault()
+                    .CreateScoped(DriveService.Scope.Drive);
+
+                // Create Drive API service.
+                var service = new DriveService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Drive API Snippets"
+                });
+
+                // Upload file photo.jpg on drive.
+                var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = "photo.jpg"
+                };
+                FilesResource.CreateMediaUpload request;
+                // Create a new file on drive.
+                using (var stream = new FileStream(filePath,
+                           FileMode.Open))
+                {
+                    // Create a new file, with metadata and stream.
+                    request = service.Files.Create(
+                        fileMetadata, stream, "image/jpeg");
+                    request.Fields = "id";
+                    request.Upload();
+                }
+
+                var file = request.ResponseBody;
+                // Prints the uploaded file id.
+                Console.WriteLine("File ID: " + file.Id);
+                return file.Id;
+            }
+            catch (Exception ex)
+            {
+                // TODO(developer) - handle error appropriately
+                if (ex is AggregateException)
+                {
+                    Console.WriteLine("Credential Not found");
+                }
+                else if (ex is FileNotFoundException)
+                {
+                    Console.WriteLine("File not found");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ APIGoogleDriveDownloader/Controllers/FileDownloader.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace APIGoogleDriveDownloader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileDownloader : ControllerBase
+    {
+        public async Task<IActionResult> DowloadFile(IFormFile uploadedFile)
+        {
+            try
+            {
+                if (uploadedFile != null && uploadedFile.Length > 0)
+                {
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + uploadedFile.FileName;//Генерация уникального имени
+                    string filePath = Path.Combine("~/TemporaryStorage/", uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await uploadedFile.CopyToAsync(fileStream);
+                    }
+                    // В этой точке файл сохранен на сервере в указанной папке по пути filePath.
+
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest("File is null");
+                }
+                
+            }
+            catch (Exception ex )
+            {
+                return BadRequest($"Error {ex}");
+                
+            }           
+       
+        }
+    }
+}
